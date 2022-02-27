30f5c9b2e6aa44060b664bdf2b72b690ed68fc6d
Ian Renton <ian@ianrenton.com>
2022-02-27 15:07:22 +0000

    Take data from a real WSJT-X WSPR log file

--- code/code.js
+++ code/code.js
@@ -1,4 +1,7 @@
-//import GreenScreen from "GreenScreen.js";
+// Own station properties
+var owncall = "2E0UXV";
+var owngrid = "IO90bs";
+var maxspots = 100;
 
 // Set up Cesium
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2ZWVjNWJhYS1hNjUyLTRkYWEtODE5MC1hMWFkYWQ0NzBhYTEiLCJpZCI6NjkxMDEsImlhdCI6MTYzMzE2MTI2OX0._M65C5gcKo2ou4xpXbbcS6JC6hobPcPQ7Cos4VOxdEE';
@@ -51,16 +54,17 @@ viewer.camera.setView({
 });
 
 // Orbit this point
-viewer.clock.onTick.addEventListener(function(clock) {
+var unsubscribeGlobeSpinEventListener = viewer.clock.onTick.addEventListener(function(clock) {
   viewer.scene.camera.rotateRight(0.005);
 });
 
-// Grid square to lat long util
+// Define grid square to lat long util
 // from https://gist.github.com/DrPaulBrewer/4279e9d234a1bd6dd3c0
 gridSquareToLatLon = function(grid, obj){
+  grid = grid.toUpperCase();
   var returnLatLonConstructor = (typeof(LatLon)==='function');
   var returnObj = (typeof(obj)==='object');
-  var lat=0.0,lon=0.0,aNum="a".charCodeAt(0),numA="A".charCodeAt(0);
+  var lat=0.0,lon=0.0,numA="A".charCodeAt(0);
   function lat4(g){
     return 10*(g.charCodeAt(1)-numA)+parseInt(g.charAt(3))-90;
   }
@@ -71,9 +75,9 @@ gridSquareToLatLon = function(grid, obj){
   if (/^[A-X][A-X][0-9][0-9]$/.test(grid)){
     lat = lat4(grid)+0.5;
     lon = lon4(grid)+1;
-  } else if (/^[A-X][A-X][0-9][0-9][a-x][a-x]$/.test(grid)){
-    lat = lat4(grid)+(1.0/60.0)*2.5*(grid.charCodeAt(5)-aNum+0.5);
-    lon = lon4(grid)+(1.0/60.0)*5*(grid.charCodeAt(4)-aNum+0.5);
+  } else if (/^[A-X][A-X][0-9][0-9][A-X][A-X]$/.test(grid)){
+    lat = lat4(grid)+(1.0/60.0)*2.5*(grid.charCodeAt(5)-numA+0.5);
+    lon = lon4(grid)+(1.0/60.0)*5*(grid.charCodeAt(4)-numA+0.5);
   } else throw "gridSquareToLatLon: invalid grid: "+grid;
   if (returnLatLonConstructor) return new LatLon(lat,lon);
   if (returnObj){
@@ -84,41 +88,58 @@ gridSquareToLatLon = function(grid, obj){
   return [lat,lon];
 };
 
+// Draw receiving station label
+var ownlatlon = gridSquareToLatLon(owngrid);
+viewer.entities.add({ position: Cesium.Cartesian3.fromDegrees(ownlatlon[1], ownlatlon[0]), label: { text: owncall, font: "16px Arial", fillColor: Cesium.Color.WHITE, outlineColor: Cesium.Color.BLACK, outlineWidth: 4, style: Cesium.LabelStyle.FILL_AND_OUTLINE } });
+
 // Fetch data
 $.ajax({
-  url: "data.csv",
+  url: "data/ALL_WSPR.TXT",
   success: async function(result) {
     // Variable setup
     var markedCallsigns = [];
     var tableContent = "";
 
-    // Parse the CSV
-    data = $.csv.toArrays(result);
-    data.forEach(row => {
-      // Extract data for the spot
-      var time = moment.unix(row[1]);
-      var receiver = row[2];
-      var receiverGrid = row[3];
-      var receiverLL = gridSquareToLatLon(receiverGrid);
-      var transmitter = row[6];
-      var transmitterGrid = row[7];
-      var transmitterLL = gridSquareToLatLon(transmitterGrid);
+    // Parse the file
+    data = result.split('\n');
 
-      // Draw a line for the spot
-      viewer.entities.add({ polyline: { positions: Cesium.Cartesian3.fromDegreesArray([transmitterLL[1], transmitterLL[0], receiverLL[1], receiverLL[0]]), width: 2} });
+    // Reverse the order so start from the most recent
+    data.reverse();
+    data.forEach(line => {
+      // Identify lines that represent receives, not transmits
+      if (line.charAt(6) == " ") {
+        console.log(line);
 
-      // Draw points for the callsigns if not already done
-      if (!markedCallsigns.includes(transmitter)) {
-        viewer.entities.add({ position: Cesium.Cartesian3.fromDegrees(transmitterLL[1], transmitterLL[0]), label: { text: transmitter, font: "16px Arial", fillColor: Cesium.Color.WHITE, outlineColor: Cesium.Color.BLACK, outlineWidth: 4, style: Cesium.LabelStyle.FILL_AND_OUTLINE } });
-        markedCallsigns.push(transmitter);
+        // Extract data for the spot
+        var time = line.substring(7, 11);
+        var rxSNR = line.substring(12, 15).trim();
+        var wsprData = line.substring(35, 58).trim();
+        var wsprDataSplit = wsprData.split(/\s+/);
+
+        // Only parse lines where we have a call, grid and power - multipart messages are ignored for now
+        console.log(wsprDataSplit.length);
+        if (wsprDataSplit.length == 3) {
+          var txcall = wsprDataSplit[0];
+          var txgrid = wsprDataSplit[1];
+          var txlatlon = gridSquareToLatLon(txgrid);
+          var txPower = wsprDataSplit[2];
+
+          // Tidy up tx call for display and reject anything wrong-looking
+          txcall = txcall.replaceAll("<", "").replaceAll(">", "");
+          if (txcall != "..." && txcall != "" && !txcall.startsWith("0")) {
+
+            // If this is a new callsign and we have space for more, draw a label and line on the globe, then add a line to the table
+            if (!markedCallsigns.includes(txcall) && markedCallsigns.length < maxspots) {
+              viewer.entities.add({ position: Cesium.Cartesian3.fromDegrees(txlatlon[1], txlatlon[0]), label: { text: txcall, font: "16px Arial", fillColor: Cesium.Color.WHITE, outlineColor: Cesium.Color.BLACK, outlineWidth: 4, style: Cesium.LabelStyle.FILL_AND_OUTLINE } });
+              viewer.entities.add({ polyline: { positions: Cesium.Cartesian3.fromDegreesArray([txlatlon[1], txlatlon[0], ownlatlon[1], ownlatlon[0]]), width: 2} });
+
+              tableContent += "<tr><td>" + time + "</td><td>" + txcall + "</td><td>" + txgrid + "</td><td>" + txPower + "</td><td>" + rxSNR + "</td></tr>";
+
+              markedCallsigns.push(txcall);
+            }
+          }
+        }
       }
-      if (!markedCallsigns.includes(receiver)) {
-        viewer.entities.add({ position: Cesium.Cartesian3.fromDegrees(receiverLL[1], receiverLL[0]), label: { text: receiver, font: "16px Arial", fillColor: Cesium.Color.WHITE, outlineColor: Cesium.Color.BLACK, outlineWidth: 4, style: Cesium.LabelStyle.FILL_AND_OUTLINE } });
-        markedCallsigns.push(receiver);
-      }
-
-      // Add data to table
-      tableContent += "<tr><td>" + time.format("hh:mm:ss") + "</td><td>" + transmitter + "</td><td>" + transmitterGrid + "</td><td>" + receiver + "</td><td>" + receiverGrid + "</td></tr>";
     });
 
     // Update table in DOM
@@ -126,3 +147,11 @@ $.ajax({
   }
 });
 
+// On user mouse down on globe, stop automatic rotation
+var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+handler.setInputAction(
+    function(click) {
+      unsubscribeGlobeSpinEventListener();
+    },
+    Cesium.ScreenSpaceEventType.LEFT_DOWN
+);
